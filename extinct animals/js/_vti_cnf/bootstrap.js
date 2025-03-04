8bbd140996f0cab0d19016a9bbd29cb008cc60a1
Riya-Kharade <riyasunilkharade.vit@gmail.com>
2025-03-04 09:55:23 +0530

    Add files via upload

---
+++ extinct animals/js/_vti_cnf/bootstrap.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|RIYASUNIL\\riyas
+vti_modifiedby:SR|RIYASUNIL\\riyas
+vti_timelastmodified:TR|28 Aug 2023 03:43:13 -0000
+vti_timecreated:TR|20 Jul 2023 04:12:21 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|my-friends.html guide.html enquiry.html contact-us.html my-family.html template.dwt skills.html qualification.html IT\\ 2023.html about-us.html gallary.html information.html index.html homepage.html
+vti_nexttolasttimemodified:TR|20 Jul 2023 04:12:21 -0000
+vti_cacheddtm:TX|28 Aug 2023 03:43:13 -0000
+vti_filesize:IR|145007
